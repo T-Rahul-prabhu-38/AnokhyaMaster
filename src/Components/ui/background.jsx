276ec54cfc9fb6dd8a24edd5a1472af08511d53f
t rahul prabhu <trahulprabhu38@gmail.com>
2024-10-28 13:05:22 +0530

    done

--- src/Components/ui/background.jsx
+++ src/Components/ui/background.jsx
@@ -22,9 +22,9 @@ export const AuroraBackground = ({
             //   I'm sorry but this is what peak developer performance looks like // trigger warning
             className={cn(
               `
-          [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
+          [--[#020617]-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
           [--dark-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
-          [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
+          [--aurora:repeating-linear-gradient(100deg,var(--[#020617]-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
           [background-image:var(--white-gradient),var(--aurora)]
           dark:[background-image:var(--dark-gradient),var(--aurora)]
           [background-size:300%,_200%]
@@ -46,3 +46,5 @@ export const AuroraBackground = ({
     </main>
   );
 };
+
+// bg-[#020617]
